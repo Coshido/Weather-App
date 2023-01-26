3bf646b056694e6b0baf22b6db7b52287064e61c
Coshido <coshido@gmail.com>
2023-01-26 16:14:58 +0100

    add async funct to get data

--- javascript.js
+++ javascript.js
@@ -1,31 +1,55 @@
 import { format, addDays, parseISO } from "https://cdn.skypack.dev/date-fns";
 
 let startDate = format(new Date(), "yyyy-MM-dd");
-let endDate = format(addDays(parseISO(startDate), 7), "yyyy-MM-dd");
-console.log(startDate, " --- ", endDate);
+let endDate = format(addDays(parseISO(startDate), 8), "yyyy-MM-dd");
 
-let cityName = "Bologna";
 //const geocodeAPI = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1`;
-
-let latitude = "44.49";
-let longitude = "11.33";
-const weatherAPI = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
+//const weatherAPI = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`;
 
 async function getCoordinates(cityName) {
   const geocodeAPI = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1`;
   let response = await fetch(geocodeAPI);
-  let geocodeData = await response.json();
-  console.log(geocodeData);
-  if (!geocodeData.results) {
+  let data = await response.json();
+  if (!data.results) {
     console.log(`No city was found with the name of: ${cityName}`);
   } else {
-    console.log(geocodeData.results[0]);
-    return geocodeData.results[0];
+    return data.results[0];
   }
 }
 
-getCoordinates("Bologna").catch(alert);
+async function getWeather(geocodeDataObject) {
+  let latitude = geocodeDataObject.latitude;
+  let longitude = geocodeDataObject.longitude;
+  const weatherAPI = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&start_date=${startDate}&end_date=${endDate}&current_weather=true`;
+  let response = await fetch(weatherAPI);
+  let data = await response.json();
+  return data;
+}
 
+async function getData(cityName) {
+  const geocodeData = await getCoordinates(cityName);
+  console.log("geocode:", geocodeData);
+  const weatherData = await getWeather(geocodeData);
+  console.log("weather:", weatherData);
+  let weatherArray = [];
+  for (let i = 0; i < 8; i++) {
+    let dayDate = weatherData.daily.time[i];
+    let weatherCode = weatherData.daily.weathercode[i];
+    let tempMin = weatherData.daily.temperature_2m_min[i];
+    let temMax = weatherData.daily.temperature_2m_max[i];
+    let weather = weatherFactory(dayDate, weatherCode, tempMin, temMax);
+    weatherArray.push(weather);
+  }
+  weatherArray[0].currentTemp = weatherData.current_weather.temperature;
+  let currentTime = weatherData.current_weather.time.split("T");
+  weatherArray[0].currentTime = currentTime[1];
+  console.log("weather array:", weatherArray);
+}
+getData("Bologna");
+
+const weatherFactory = (dayDate, weatherCode, tempMin, tempMax) => {
+  return { dayDate, weatherCode, tempMin, tempMax };
+};
 //footer
 
 function createFooter() {
